1d92724ee74c8acc250fe25b2959ac7af3205978
“alerenna” <alessandrorennafm@gmail.com>
2025-03-26 22:16:52 +0800

    Add a form for entering new items and view the list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const postList = [
   {
     id: 1,
@@ -36,37 +38,109 @@ const postList = [
   }
 ];
 
-
 export default function App() {
+
+  const [newPost, setNewPost] = useState({
+    title: '',
+    author: '',
+    date: '',
+    content: ''
+  })
+
+  const [posts, setPosts] = useState(postList)
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    console.log(newPost);
+
+    setPosts([...posts, newPost])
+
+    console.log(posts);
+
+  }
+
+
+
   return (
     <>
-      {postList.map((post) => (
-        <div className="container">
-          <h1 className=" d-flex text-center p-2 align-items-center justify-content-center">{post.title}</h1>
-        </div>
-      ))}
 
-      <form className="row g-3 needs-validation p-5 ">
+      <form className="p-5" onSubmit={handleFormSubmit}>
 
-        <div class="mb-3">
-          <label for="" class="form-label">Nuovo articolo</label>
+        <div className="mb-3">
+
           <input
             type="text"
-            class="form-control"
-            name="new_article"
-            id="new_article"
+            className="form-control"
+            name="new_article_title"
+            id="new_article_title"
             aria-describedby="newArticleHelpId"
             placeholder="Il tuo titolo qui"
+            value={newPost.title}
+            onChange={e => setNewPost({ ...newPost, title: e.target.value })}
           />
-          <small id="newArticleHelpId" class="form-text text-muted">Inserisci il titolo del nuovo articolo</small>
+          <small id="newArticleHelpId" className="form-text text-muted">Inserisci il titolo del nuovo articolo</small>
         </div>
 
-        <div className="col-12">
+        <div className="mb-3">
+
+          <input
+            type="text"
+            className="form-control"
+            name="new_article_author"
+            id="new_article_author"
+            aria-describedby="newArticleHelpId"
+            placeholder="L'autore qui"
+            value={newPost.author}
+            onChange={e => setNewPost({ ...newPost, author: e.target.value })}
+          />
+          <small id="newArticleHelpId" className="form-text text-muted">L'autore qui</small>
+        </div>
+
+        <div className="mb-3">
+
+          <input
+            type="text"
+            className="form-control"
+            name="new_article_date"
+            id="new_article_date"
+            aria-describedby="newArticleHelpId"
+            placeholder="La data qui"
+            value={newPost.date}
+            onChange={e => setNewPost({ ...newPost, date: e.target.value })}
+          />
+          <small id="newArticleHelpId" className="form-text text-muted">La data qui</small>
+        </div>
+
+        <div className="mb-3">
+
+          <input
+            type="text"
+            className="form-control"
+            name="new_article_content"
+            id="new_article_content"
+            aria-describedby="newArticleHelpId"
+            placeholder="Il tuo contenuto qui"
+            value={newPost.content}
+            onChange={e => setNewPost({ ...newPost, content: e.target.value })}
+          />
+          <small id="newArticleHelpId" className="form-text text-muted">Il tuo contenuto qui</small>
+        </div>
+
+        <div className="col-12 d-flex justify-content-center">
           <button className="btn btn-primary" type="submit">Inserisci nuovo articolo</button>
         </div>
       </form>
 
 
+      <ul className="list-group">
+        {posts.map((post, index) => (
+          <li key={`post-${index}`}
+            className=" d-flex text-center p-2 align-items-center justify-content-center">
+            {post.title}
+          </li>
+        ))}
+      </ul>
 
     </>
 
